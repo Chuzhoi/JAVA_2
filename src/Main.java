0aa3d10e4875813dcbcf5dfe27d0a4638efbb26d
Konstantin <parnishkafin@gmail.com>
2023-03-05 15:32:26 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 20;    // делитель
+        int ticket = 7270;   // стоимость билета
+        int bonus = ticket/amount;  // бонус
+        System.out.println("Начислено миль:" + bonus);
+    }
+}
